e8409a2b3f5a60a0652fa79b03b34b8d78dad496
Benson Ly <bwly314@gmail.com>
2018-09-07 21:33:35 -0700

    Added basic messages for fetching paddlers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PaddlersComponent } from './paddlers/paddlers.component';
 import { PaddlerDetailComponent } from './paddler-detail/paddler-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         PaddlersComponent,
-        PaddlerDetailComponent
+        PaddlerDetailComponent,
+        MessagesComponent
     ],
     imports: [
         BrowserModule,
--- src/app/paddler.service.ts
+++ src/app/paddler.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Paddler } from './paddler';
 import { PADDLERS } from './mock-paddlers';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaddlerService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getPaddlers(): Observable<Paddler[]> {
+    this.messageService.add('PaddlerService: fetched paddler');
     return of(PADDLERS);
   }
 }
