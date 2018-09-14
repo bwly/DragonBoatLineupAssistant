17121d3abb2e2a183d5ad391ebc99cbd254e32c4
Benson Ly <bwly314@gmail.com>
2018-09-14 14:00:50 -0700

    Teams are now sorted when they appear in the dropdown

--- src/app/add-paddler/add-paddler.component.ts
+++ src/app/add-paddler/add-paddler.component.ts
@@ -13,7 +13,8 @@ export class AddPaddlerComponent implements OnInit {
   teams: Team[];
   model = new Paddler(null, null, null, null, null);
 
-  constructor(private paddlerService: PaddlerService, private teamService: TeamService) { }
+  constructor(private paddlerService: PaddlerService,
+              private teamService: TeamService) { }
 
   ngOnInit() {
     this.getTeams();
@@ -24,6 +25,19 @@ export class AddPaddlerComponent implements OnInit {
   }
 
   getTeams(): void {
-    this.teamService.getTeams().subscribe(teams => this.teams = teams);
+    this.teamService.getTeams().subscribe(teams => this.teams = teams.sort(this.sortTeamsByName));
+  }
+
+  sortTeamsByName(teamOne: Team, teamTwo: Team): number {
+    let returnValue;
+
+    if (teamOne.name < teamTwo.name) {
+      returnValue = -1;
+    } else if (teamOne.name > teamTwo.name) {
+      returnValue = 1;
+    } else {
+      returnValue = 0;
+    }
+    return returnValue;
   }
 }
