ad17ac4330b197517d36c2c04bb01b016525c612
Benson Ly <bwly314@gmail.com>
2018-09-21 11:06:11 -0700

    Got rid of unneeded dependencies

--- src/app/services/team.service.spec.ts
+++ src/app/services/team.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { TeamService } from './team.service';
 import { AngularFireDatabase } from 'angularfire2/database';
